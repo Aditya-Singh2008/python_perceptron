ff7ff2421e3640e429a9f2d81c43ada716251764
Aditya Singh <adityasinghaditya2008@gmail.com>
2024-03-11 01:24:57 -0700

    Using Numpy now

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from perceptron import Perceptron
+import os
+import numpy as np
+from perceptron import *
 
 def main():
-    ptron = Perceptron(2, 2)
+    inp = [1.0, 2.0, 3.0, 4.0, 5.0]
+    out = [1.0, 2.0, 3.0, 4.0, 5.0]
+    ptron = Perceptron(1, 1, inp, out)
 
-    ptron.initialize_weights();
-    ptron.print_weights();
+    ptron.initialize_weights()
+    ptron.initialize_wsums()
+    ptron.print_all()
+
 main()
--- perceptron.py
+++ perceptron.py
@@ -2,11 +2,22 @@ import numpy as np
 import random
 
 class Perceptron:
-    def __init__(self, input_count, output_count):
+    def __init__(self, input_count, output_count, inputs, eoutputs):
         self.input_count = input_count
+        self.output_count = output_count
+        self.inputs = np.array(inputs, dtype=float)
+        self.eoutputs = np.array(eoutputs, dtype=float)
+        self.rng = np.random.default_rng(2)
 
     def initialize_weights(self):
-        self.weights = [random.random()] * self.input_count
-    def print_weights(self):
-        print(self.weights[0])
-        
+        self.weights = self.rng.random(self.input_count)
+    def initialize_wsums(self):
+        self.wsums = np.zeros(self.input_count, dtype=float)
+
+
+
+    def print_all(self):
+        print(self.inputs)
+        print(self.eoutputs)
+        print(self.weights)
+        print(self.wsums)
