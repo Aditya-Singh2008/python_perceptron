78b7c9e4815e64ed1325feb4f208da7e4f4307d3
Aditya Singh <adityasinghaditya2008@gmail.com>
2024-03-11 22:42:45 -0700

    Expaneded numpy usage and experementing w/ dot product for weighted sum

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import numpy as np
 from perceptron import *
 
 def main():
-    inp = [1.0, 2.0, 3.0, 4.0, 5.0]
-    out = [1.0, 2.0, 3.0, 4.0, 5.0]
-    ptron = Perceptron(1, 1, inp, out)
+    inp = np.arange(1, 5, dtype=float)
+    out = np.arange(1, 5, dtype=float)
+    ptron = Perceptron(1, 1)
+
+    ptron.passdata(inp, out)
 
     ptron.initialize_weights()
     ptron.initialize_wsums()
--- perceptron.py
+++ perceptron.py
@@ -1,20 +1,27 @@
 import numpy as np
-import random
 
 class Perceptron:
-    def __init__(self, input_count, output_count, inputs, eoutputs):
+    def __init__(self, input_count, output_count):
         self.input_count = input_count
         self.output_count = output_count
-        self.inputs = np.array(inputs, dtype=float)
-        self.eoutputs = np.array(eoutputs, dtype=float)
         self.rng = np.random.default_rng(2)
 
+    def passdata(self, inputs, eoutputs):
+        self.inputs = np.array(inputs, dtype=float)
+        self.eoutputs = np.array(eoutputs, dtype=float)
+
     def initialize_weights(self):
-        self.weights = self.rng.random(self.input_count)
+        self.weights = self.rng.random((self.input_count, self.output_count))
+    
     def initialize_wsums(self):
         self.wsums = np.zeros(self.input_count, dtype=float)
-
-
+    
+    def forwardpass(self):
+        self.wsums = self.dotprod(self.inputs, self.weights)
+    
+    def dotprod(self):
+        return np.dot(self.weights, self.inputs)
+    
 
     def print_all(self):
         print(self.inputs)
