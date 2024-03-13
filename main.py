Fixed dot product calculation to be consistant with neural net

main.py
@@ -3,14 +3,15 @@ import numpy as np
 from perceptron import *
 
 def main():
-    inp = np.arange(1, 5, dtype=float)
-    out = np.arange(1, 5, dtype=float)
-    ptron = Perceptron(1, 1)
+    inp = np.arange(1, 3, dtype=float)
+    out = np.arange(1, 3, dtype=float)
+    ptron = Perceptron(2, 2)
 
     ptron.passdata(inp, out)
 
     ptron.initialize_weights()
     ptron.initialize_wsums()
+    ptron.forwardpass()
     ptron.print_all()
 
 main()

perceptron.py
@@ -4,23 +4,21 @@ class Perceptron:
     def __init__(self, input_count, output_count):
         self.input_count = input_count
         self.output_count = output_count
-        self.rng = np.random.default_rng(2)
+        self.rng = np.random.default_rng(10)
 
     def passdata(self, inputs, eoutputs):
         self.inputs = np.array(inputs, dtype=float)
         self.eoutputs = np.array(eoutputs, dtype=float)
 
     def initialize_weights(self):
-        self.weights = self.rng.random((self.input_count, self.output_count))
+        self.weights = self.rng.random((self.output_count, self.input_count))
     
     def initialize_wsums(self):
         self.wsums = np.zeros(self.input_count, dtype=float)
     
     def forwardpass(self):
-        self.wsums = self.dotprod(self.inputs, self.weights)
+        self.wsums = np.dot(self.weights, self.inputs)
     
-    def dotprod(self):
-        return np.dot(self.weights, self.inputs)
     
 
     def print_all(self):