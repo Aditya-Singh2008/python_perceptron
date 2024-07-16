3742effae405c0dce6190305b87f5945695e5b8d
Aditya Singh <adityasinghaditya2008@gmail.com>
2024-07-15 19:17:48 -0700

    Added bias vector

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from perceptron import *
 def main():
     learningrate = 0.1
     inp = np.arange(1, 3, dtype=float)
-    out = np.arange(1, 3, dtype=float)
-    ptron = Perceptron(1, 1)
+    out = [1]#np.arange(1, 3, dtype=float)
+    ptron = Perceptron(2, 1)
 
     ptron.initialize_weights()
     ptron.passdata(inp, out, learningrate)
@@ -15,6 +15,5 @@ def main():
         ptron.forwardpass()
         ptron.print_all()
         ptron.backprop()
-
 
 main()
--- perceptron.py
+++ perceptron.py
@@ -4,7 +4,7 @@ class Perceptron:
     def __init__(self, input_count, output_count):
         self.input_count = input_count
         self.output_count = output_count
-        self.rng = np.random.default_rng(1)
+        self.rng = np.random.default_rng(9)
 
     def passdata(self, inputs, outputs, learningrate):
         self.inputs = np.array(inputs, dtype=float)
@@ -13,26 +13,34 @@ class Perceptron:
 
     def initialize_weights(self):
         self.weights = self.rng.random((self.output_count, self.input_count))
-        print("weight:", self.weights)
+        self.bias = self.rng.random((self.output_count, 1))
         self.dweights = np.zeros((self.output_count, self.input_count), dtype=float)
+        self.dbias = np.zeros((self.output_count, 1), dtype=float)     
     
     def forwardpass(self):
         self.wsums, self.dwvdot = self.vdot(self.weights, self.inputs)
+        self.wsums, self.daddbias = self.addbias(self.wsums, self.bias)
         self.wsums, self.dwsigmoid = self.sigmoid(self.wsums)
         self.mse, self.dwmse = self.meansquarederr(self.outputs, self.wsums, self.output_count)
 
-        self.dweights = self.dwvdot * self.dwsigmoid * self.dwmse
+        self.dweights = self.dwvdot * self.daddbias * self.dwsigmoid * self.dwmse
+        self.dbias = self.daddbias * self.dwmse * self.dwsigmoid
 
     def backprop(self):
         self.weights -= (self.learningrate * self.dweights)
+        self.bias -= (self.learningrate * self.dbias)
 
     def vdot(self, weights, inputs):
         dot = np.dot(weights, inputs)
-        print("dot: ", dot)
-
         def dvdot(x):
             return np.transpose(x)
         return  dot, dvdot(inputs)
+    
+    def addbias(self, weights, bias):
+        modified = weights + bias
+        def daddbias(x):
+            return np.ones_like(x)
+        return modified, daddbias(bias)
     
     def sigmoid(self, array):
         act = (1/(1+ np.exp(-array)))
@@ -50,6 +58,7 @@ class Perceptron:
         print(self.inputs)
         print(self.outputs)
         print("w: ", self.weights)
+        print("b: ", self.bias)
         print(self.wsums, np.size(self.wsums))
         print("mse: ", self.mse)
         print(self.dweights)
