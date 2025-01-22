043c6de04cb17f37938333e9af2b901f6563220b
Aditya Singh <adityasinghaditya2008@gmail.com>
2025-01-22 13:10:55 -0800

    Fixed bias update

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from perceptron import *
 def main():
     learningrate = 0.1
     inp = np.arange(1, 3, dtype=float)
-    out = [1]#np.arange(1, 3, dtype=float)
-    ptron = Perceptron(2, 1)
+    out = np.arange(1, 3, dtype=float)
+    ptron = Perceptron(2, 2)
 
     ptron.initialize_weights()
     ptron.passdata(inp, out, learningrate)
 
-    for i in range(5):
+    for i in range(100):
         ptron.forwardpass()
         ptron.print_all()
         ptron.backprop()
--- perceptron.py
+++ perceptron.py
@@ -4,7 +4,7 @@ class Perceptron:
     def __init__(self, input_count, output_count):
         self.input_count = input_count
         self.output_count = output_count
-        self.rng = np.random.default_rng(9)
+        self.rng = np.random.default_rng(10)
 
     def passdata(self, inputs, outputs, learningrate):
         self.inputs = np.array(inputs, dtype=float)
@@ -15,7 +15,7 @@ class Perceptron:
         self.weights = self.rng.random((self.output_count, self.input_count))
         self.bias = self.rng.random((self.output_count, 1))
         self.dweights = np.zeros((self.output_count, self.input_count), dtype=float)
-        self.dbias = np.zeros((self.output_count, 1), dtype=float)     
+        self.dbias = np.zeros((self.output_count, 1), dtype=float)
     
     def forwardpass(self):
         self.wsums, self.dwvdot = self.vdot(self.weights, self.inputs)
@@ -24,7 +24,7 @@ class Perceptron:
         self.mse, self.dwmse = self.meansquarederr(self.outputs, self.wsums, self.output_count)
 
         self.dweights = self.dwvdot * self.daddbias * self.dwsigmoid * self.dwmse
-        self.dbias = self.daddbias * self.dwmse * self.dwsigmoid
+        self.dbias = np.sum(self.daddbias * self.dwsigmoid * self.dwmse, axis=1, keepdims=True)
 
     def backprop(self):
         self.weights -= (self.learningrate * self.dweights)
@@ -37,10 +37,10 @@ class Perceptron:
         return  dot, dvdot(inputs)
     
     def addbias(self, weights, bias):
-        modified = weights + bias
+        modified = weights * bias
         def daddbias(x):
             return np.ones_like(x)
-        return modified, daddbias(bias)
+        return modified, daddbias(self.bias)
     
     def sigmoid(self, array):
         act = (1/(1+ np.exp(-array)))
